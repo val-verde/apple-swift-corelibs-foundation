2943eeef25b3967160146cc58543c69e20bc7ff0
Aaditya Chandrasekhar <aadityac@gmail.com>
2022-10-05 02:03:41 +0000

    Add definition for ERROR_CANCELLED in Sources/Foundation/NSError.swift.

--- Sources/Foundation/NSError.swift
+++ Sources/Foundation/NSError.swift
@@ -1316,6 +1316,7 @@ extension POSIXError {
     /// Operation canceled.
     public static var ECANCELED: POSIXError.Code {
 #if os(Windows)
+        let ERROR_CANCELLED = 1223
         return POSIXError.Code(rawValue: Int32(ERROR_CANCELLED))!
 #else
         return .ECANCELED
